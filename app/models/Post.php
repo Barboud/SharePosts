debb9e0336e68360bdb2c45d6aeb3bdf780e31f3
Barboud <barboud@gmail.com>
2020-10-13 21:55:20 +0300

    Last commit

--- app/models/Post.php
+++ app/models/Post.php
@@ -22,4 +22,62 @@ class Post {
 
         return $results;
     }
+
+    public function addPost($data){
+        $this->db->query('INSERT INTO posts (title, user_id, body) VALUES (:title, :user_id, :body)');
+
+        $this->db->bind(':title' , $data['title']);
+        $this->db->bind(':user_id' , $data['user_id']);
+        $this->db->bind(':body' , $data['body']);
+
+        if ($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getPostById($id){
+        $this->db->query('SELECT * FROM posts WHERE id = :id');
+        $this->db->bind(':id' , $id);
+        $row = $this->db->single();
+
+        return $row;
+    }
+
+    public function updatePost($data){
+        $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+        // Bind values
+        $this->db->bind(':id', $data['id']);
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':body', $data['body']);
+
+        // Execute
+        if($this->db->execute()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete($id){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Get existing post from model
+            $post = $this->postModel->getPostById($id);
+
+            // Check for owner
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }
+
+            if($this->postModel->deletePost($id)){
+                flash('post_message', 'Post Removed');
+                redirect('posts');
+            } else {
+                die('Something went wrong');
+            }
+        } else {
+            redirect('posts');
+        }
+    }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -44,6 +44,14 @@ class User {
         } else {
             return false;
         }
-
     }
+
+    public function getUserById($id){
+        $this->db->query('SELECT * FROM users WHERE id = :id');
+        $this->db->bind(':id' , $id);
+        $row = $this->db->single();
+
+        return $row;
+    }
+
 }
