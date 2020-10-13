9db007ef26f4a23e59a60b8070bcfec33668234a
Barboud <barboud@gmail.com>
2020-10-13 21:55:30 +0300

    Last commit

--- shareposts.sql
+++ shareposts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 11, 2020 at 07:57 PM
+-- Generation Time: Oct 13, 2020 at 06:54 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -32,11 +32,11 @@ DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` text NOT NULL,
+  `title` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `body` text CHARACTER SET latin1 NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `posts`
@@ -44,7 +44,8 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `created_at`) VALUES
 (1, 7, 'test', 'just for test', '2020-10-11 21:49:32'),
-(2, 7, 'just for test', 'test test testtesttest test testtest test', '2020-10-11 21:49:32');
+(2, 7, 'just for test', 'test test testtesttest test testtest test', '2020-10-11 21:49:32'),
+(3, 7, 'To Delete', 'just loml 654 645 6456 546 13 1 Hi eery one', '2020-10-13 20:39:00');
 
 -- --------------------------------------------------------
 
@@ -55,19 +56,20 @@ INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `created_at`) VALUES
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
-(7, 'Salem Barboud', 'barboud@gmail.com', '$2y$10$YsHkadJr26xBhAcPUA5ozeY4VRkbminXpzw0LWKfbsAz33Ae7gtQu', '2020-10-11 21:04:32');
+(7, 'Salem Barboud', 'barboud@gmail.com', '$2y$10$YsHkadJr26xBhAcPUA5ozeY4VRkbminXpzw0LWKfbsAz33Ae7gtQu', '2020-10-11 21:04:32'),
+(9, 'Fahd Salem', 'fahd@fahd.com', '$2y$10$kJuCVmP7Fak2A1xUA21.RO5tRuasrIOR2rzBnX6vyf9lAjpf0WzPG', '2020-10-13 21:34:20');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
